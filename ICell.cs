2697438d1ff6ca0abb72cadffe34ad5761795dea
Caleb Eaton <kryto627@gmail.com>
2020-11-04 04:45:49 -0500

    Grid & Cell Interfaces
    Added a grid and cell interface.

---
+++ ICell.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sudoku {
+
+    public interface ICell<T> {
+
+        bool IsFinal { get; }
+
+        T State { get; }
+
+        T[] PossibleStates { get; }
+
+        void Populate(IGrid<T> grid, int x, int y, T[] registerStates);
+
+        void SetFinalState(int index);
+    }
+}
---
+++ IGrid.cs
@@ -0,0 +1,14 @@
+﻿
+namespace Sudoku {
+
+    public interface IGrid<T> {
+
+        int Size { get; }
+
+        ICell<T>[,] Cells { get; }
+
+        ICell<T> GetCell(int x, int y);
+
+        void Solve();
+    }
+}
